df71dfe3ed944dc78e1e83200208baa2b8e3eb50
spark <ses8621@gmail.com>
2019-07-04 14:37:36 -0700

    Applied CSS to top portion of login page

--- frontend/components/nav_top/session_form/login_form.jsx
+++ frontend/components/nav_top/session_form/login_form.jsx
@@ -43,37 +43,59 @@ class LogInForm extends React.Component {
 
     // single form version
     return (
-      <div>
+      <div className="signin-page-entire">
+        <div className="signin-page">
 
-        <h1>Sign in</h1>
-        <p>to continue to SparKube</p>
+          <div className="logo-text-div rainbow">
+            <span className="char-blue">S</span>
+            <span className="char-red">p</span>
+            <span className="char-yellow">a</span>
+            <span className="char-blue">r</span>
+            <span className="char-green">K</span>
+            <span className="char-yellow">u</span>
+            <span className="char-blue">b</span>
+            <span className="char-red">e</span>
+          </div>
 
-        <form onSubmit={this.handleSubmit}>
-          {this.renderError()}
-          <label htmlFor="username">Username</label>
-          <br />
-          <input 
-            id='username'
-            type="text"
-            value={this.state.username}
-            onChange={this.update('username')} />
-          <br /><br />
+          <h1 className="signin-text-h1">
+            <span>Sign in</span>
+          </h1>
 
-          <label htmlFor="password">Password</label>
-          <br />
-          <input 
-            id='password'
-            type="password"
-            value={this.state.password}
-            onChange={this.update('password')} />
-          <br /><br />
+          <h2 className="continue-text-div">
+            <span>to continue to SparKube</span>
+          </h2>
 
-          <Link to='/signup'>Create account</Link>
+          <form
+            onSubmit={this.handleSubmit}
+            className="signin-form" >
 
-          <input 
-            type="submit"
-            value='Next' />
-        </form>
+            {this.renderError()}
+            
+            <label htmlFor="username">Username</label>
+            <br />
+            <input 
+              id='username'
+              type="text"
+              value={this.state.username}
+              onChange={this.update('username')} />
+            <br /><br />
+
+            <label htmlFor="password">Password</label>
+            <br />
+            <input 
+              id='password'
+              type="password"
+              value={this.state.password}
+              onChange={this.update('password')} />
+            <br /><br />
+
+            <Link to='/signup'>Create account</Link>
+
+            <input 
+              type="submit"
+              value='Next' />
+          </form>
+        </div>
       </div>
     );
   };
