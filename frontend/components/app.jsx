b6e86b9fedcb4406960b32e6793171259e5017b9
spark <ses8621@gmail.com>
2019-07-05 10:02:07 -0700

    Update frontend route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,18 +1,24 @@
 
 import React from 'react';
 import NavTop from './nav_top/nav_top';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import SignUpFormContainer from './nav_top/session_form/signup_form_container';
 import LogInFormContainer from './nav_top/session_form/login_form_container';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { 
+  AuthRoute, 
+  ProtectedRoute 
+  } from '../util/route_util';
 
 const App = () => {
   return (
     <div>
-      <AuthRoute exact path='/signup' component={SignUpFormContainer} />
-      <AuthRoute exact path='/login' component={LogInFormContainer} />
-      
-      <Route exact path='/' component={NavTop} />
+
+      <Switch>
+        <Route exact path='/' component={NavTop} />
+        <AuthRoute exact path='/signup' component={SignUpFormContainer} />
+        <AuthRoute exact path='/login' component={LogInFormContainer} />
+        <Redirect to='/' component={NavTop} />
+      </Switch>
     </div>
   );
 };
