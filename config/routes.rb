a3c59cc4116ffd7a6dd1a6b34c63777cdd50b5f7
spark <ses8621@gmail.com>
2019-07-05 14:24:05 -0700

    Configure active storate and migrate videos table

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :video, only: [:index, :show, :create, :update, :destroy]
   end
   
 end
