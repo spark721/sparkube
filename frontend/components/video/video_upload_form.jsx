0ed7b49ed67f60c2f951ec18f3ea34a6072c5823
spark <ses8621@gmail.com>
2019-07-08 12:29:59 -0700

    working on video_upload

--- frontend/components/video/video_upload_form.jsx
+++ frontend/components/video/video_upload_form.jsx
@@ -10,7 +10,33 @@ class VideoUploadForm extends React.Component {
       <div>
         <NavTop />
         <div className='upload-form-page'>
-          <h1>This is VideoUploadForm</h1>
+          <div className='upload-form-div'>
+            <form className='upload-form'>
+              <div className="upload-mp4-div">
+                <label htmlFor="upload-mp4"
+                       className="form-upload-icon">
+                  <i className="fas fa-play-circle"></i>
+                </label>
+                <input type="file"
+                       id="upload-mp4"
+                       accept=".mp4" />
+                       
+              </div>
+
+              
+              <div className="upload-img-div">
+                <label htmlFor="upload-img"
+                       className="form-upload-icon">
+                  <i className="fas fa-image"></i>
+                </label>
+                <input type="file"
+                       id="upload-img"
+                       accept=".png,.jpg" />
+              </div>
+
+              
+            </form>
+          </div>
         </div>
       </div>
     );
