b0be29dffb71164327b21d4147417a1d7f05ba84
spark <ses8621@gmail.com>
2019-07-08 17:50:47 -0700

    Implement preview functionality on video_upload_form

--- frontend/components/video/video_upload_form.jsx
+++ frontend/components/video/video_upload_form.jsx
@@ -5,13 +5,48 @@ import NavTop from '../nav_top/nav_top';
 
 class VideoUploadForm extends React.Component {
 
+  componentDidMount() {
+    let inputVid = document.getElementById('upload-mp4');
+    let previewVid = document.getElementById('preview-vid');
+
+    inputVid.addEventListener('change', updateVidDisplay);
+
+    function updateVidDisplay() {
+      while (previewVid.firstChild) {
+        previewVid.removeChild(previewVid.firstChild);
+      }
+
+      const curVids = inputVid.files;
+      let userVid = document.createElement('video');
+      let vidSource = document.createElement('source');
+      userVid.width = "250";
+      userVid.controls = true;
+      vidSource.type = "video/mp4";
+      vidSource.src = window.URL.createObjectURL(curVids[0]);
+      userVid.appendChild(vidSource);
+
+      previewVid.appendChild(userVid);
+    }
+
+    let inputImg = document.getElementById('upload-img');
+    let previewImg = document.getElementById('preview-img');
+
+    inputImg.addEventListener('change', updateImgDisplay);
+
+    function updateImgDisplay() {
+      while (previewImg.firstChild) {
+        previewImg.removeChild(previewImg.firstChild);
+      }
+
+      const curImgs = inputImg.files;
+      let userImg = document.createElement('img');
+      userImg.src = window.URL.createObjectURL(curImgs[0]);
+      previewImg.appendChild(userImg);
+    }
+
+  }
+
   render() {
-    // let inputVideo = document.getElementById('input')
-    // inputVideo.addEventListener('change', updateImageDisplay);
-    // function updateImageDisplay() {
-
-    // }
-
     return(
       <div>
         <NavTop />
@@ -26,10 +61,12 @@ class VideoUploadForm extends React.Component {
                 <input type="file"
                        id="upload-mp4"
                        accept="video/mp4" />
-                       
+
+              <div id="preview-vid">
+                <p>No File currently selected</p>
+              </div>
               </div>
 
-              
               <div className="upload-img-div">
                 <label htmlFor="upload-img"
                        className="form-upload-icon">
@@ -38,8 +75,11 @@ class VideoUploadForm extends React.Component {
                 <input type="file"
                        id="upload-img"
                        accept="image/png, image/jpeg" />
-              </div>
 
+                <div id="preview-img">
+                  <p>No File currently selected</p>
+                </div>
+              </div>
               
             </form>
           </div>
